9bd3833b357144514904c6c5c1e112c5b8beef02
kormarun <kormarun@gmail.com>
2021-10-02 02:48:29 +0200

    upgrade

--- Assets/Scripts/DecayAnimation.cs
+++ Assets/Scripts/DecayAnimation.cs
@@ -1,15 +1,31 @@
+using System;
 using UnityEngine;
 
 public class DecayAnimation : MonoBehaviour
 {
     public float timeAlive = 5;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        var component = GetComponent<Animator>();
+        if (component)
+        {
+            component.StartPlayback();
+        }
+    }
 
     void Update()
     {
-        transform.Rotate(Vector3.up, 360 * Time.deltaTime);
         timeAlive -= Time.deltaTime;
 
         if (timeAlive < 0)
+        {
+            transform.position -= velocity;
+            velocity += 0.098f * Vector3.up;
+        }
+
+        if (timeAlive < -10)
         {
             Destroy(gameObject);
         }
