a7d7e2ef2e9875c18f3a6328b60035264da64380
kormarun <kormarun@gmail.com>
2021-10-03 23:40:40 +0200

    maybe improve billboarding

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class Billboard : MonoBehaviour
 {
     public Camera camera;
-    
+
     void Update()
     {
-         transform.LookAt(camera.transform);
-         transform.Rotate(0, 180,0);
+        Vector3 lookAt = transform.position;
+        lookAt -= (camera.transform.position - lookAt);
+        // Debug.DrawLine(transform.position, lookAt, Color.red);
+        lookAt.y = transform.position.y;
+        // Debug.DrawLine(transform.position, lookAt, Color.green);
+        transform.LookAt(lookAt);
+        // transform.Rotate(0, 180, 0);
     }
 }
