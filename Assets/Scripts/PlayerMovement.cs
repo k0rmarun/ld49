e4bd7559a62af60500e8673e9065441c06f0a46f
kormarun <kormarun@gmail.com>
2021-10-04 22:55:52 +0200

    remove debug log

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("++++++"+PauseMenu.isPaused);
         if (Input.GetButtonDown("Cancel"))
         {
             Camera.main.GetComponent<PauseMenu>().ToggleMenu();
