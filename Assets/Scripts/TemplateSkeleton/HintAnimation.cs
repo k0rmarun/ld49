f54e71badc77bc018a116f5505beb745e23792be
Jan Schwien <jan5366@gmail.com>
2021-10-04 12:05:58 +0200

    Work on tutorial hints

---
+++ Assets/Scripts/TemplateSkeleton/HintAnimation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Hint Animation", menuName = "Template/Hint Animation")]
+public class HintAnimation : ScriptableObject
+{
+    public HintIconData[] icons;
+}
